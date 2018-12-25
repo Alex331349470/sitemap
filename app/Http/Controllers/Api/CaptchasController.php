e05dc8dcaa4bda032eedcc3bd941aa8aca3f2bc1
vagrant <vagrant@homestead>
2018-12-25 07:49:15 +0000

    set sms for app

---
+++ app/Http/Controllers/Api/CaptchasController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\Api\CaptchaRequest;
+use Illuminate\Http\Request;
+
+class CaptchasController extends Controller
+{
+    public function store(CaptchaRequest $request)
+    {
+        
+    }
+}
--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        App\Providers\EasySmsProvider::class,
         /*
          * Application Service Providers...
          */
